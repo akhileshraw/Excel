ce5a6eda281783d89accef908bea827b9a7bf3d3
deepu <deepu@DESKTOP-76O5QF6>
2021-08-09 11:12:39 +0530

    Excel +TestNG

---
+++ src/Excel_Class.java
@@ -0,0 +1,31 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class Excel_Class {
+	@Test
+ public void Excel() throws Exception {
+	 FileInputStream FileInput=new FileInputStream("E:\\Excel_File\\WriteData.xlsx");
+	 XSSFWorkbook workbook=new XSSFWorkbook(FileInput);
+	 XSSFSheet Sheet=workbook.getSheet("test");
+	 System.out.println(Sheet.getSheetName());
+	 System.out.println(Sheet.getLastRowNum());
+	 System.out.println("Before Updating Cell Data is  "+ Sheet.getRow(2).getCell(1));
+	 Reporter.log("Row data is Fetched");
+	 
+	 //Write Data to Excel File
+	 XSSFCell cell=Sheet.getRow(2).getCell(1);
+	 cell.setCellValue("Test123456");
+	 FileInput.close();
+	 FileOutputStream FileOut=new FileOutputStream("E:\\Excel_File\\WriteData.xlsx");
+	 Reporter.log("row Data is fetched");
+	 workbook.write(FileOut);
+	 System.out.println("Updated file After Write is done"+ cell.getStringCellValue());
+	 FileOut.close();
+ }
+}
